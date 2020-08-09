534fb7124c95380eac6792aca1a292d47f4de982
Brian Gibbons <brigibb.ux@gmail.com>
2020-08-08 20:03:49 -0600

    have all characters and images about to work on being able to add a card

--- cards.js
+++ cards.js
@@ -7,7 +7,7 @@
 const conatainer = document.querySelector('#container');
 
 //Get the Data
-const getGroup1 = () => {
+/*const getGroup1 = () => {
     fetch('http://swapi.dev/api/people/')
         .then(resp => resp.json())
         .then(data => {
@@ -88,7 +88,100 @@ const getGroup1 = () => {
 }
 
 
-getGroup1();
+getGroup1();*/
+
+function get30SWPeople(params) {
+    let people = [];
+    const SWPromises = [
+        fetch('https://swapi.dev/api/people/?page=1'),
+        fetch('https://swapi.dev/api/people/?page=2'),
+        fetch('https://swapi.dev/api/people/?page=3')
+    ]
+   return Promise.all(SWPromises)
+    .then(ResponsesArr => {
+        return Promise.all(
+            ResponsesArr.map(data => data.json())
+            ) 
+    })
+    .then(jsonDataArr => {
+        people = jsonDataArr.reduce(
+            (acc, data) => [...acc, ...data.results]
+            , people)
+        return people;
+    })
+  }
+  get30SWPeople().then(people => {
+    console.log(people)
+    const arrayOfPeople = people.map((person, idx) => {
+        return {
+            name: person.name,
+            gender: person.gender,
+            dob: person.birth_year,
+            id: idx,
+            height: person.height,
+            mass: person.mass,
+            eyes: person.eye_color,
+            hair: person.hair_color,
+        }
+    }  
+    )
+    console.log(arrayOfPeople)
+
+    const htmlForCards = arrayOfPeople.map(person => {
+        return `
+    <div class="scene">
+        <div class="card">
+            
+                <div class="card_face card_face-front">
+                    <div id="front-info">
+                            <img src="swimg/${person.id}.jpg">
+                            <p>${person.name}</p>
+                    </div>
+                </div>
+
+                <div class="card_face card_face-back">
+                    <div id="${person.id}back-info">
+                            <div id="back-header">
+                                <h1 id="back-h1">${person.name}</h1>
+                            </div>
+                        <div id="backblock">
+                            <div id="back-left">
+                                <p>DOB</p>
+                                <p>SEX</p>
+                                <p>HGT</p>
+                                <p>MASS</p>
+                                <p>EYES</p>
+                                <p>HAIR</p>
+                            </div>
+                            <div id="back-right">
+                                <p>${person.dob}</p>
+                                <p>${person.gender}</p>
+                                <p>${person.height}</p>
+                                <p>${person.mass}</p>
+                                <p>${person.eyes}</p>
+                                <p>${person.hair}</p>
+                            </div>
+                        </div>     
+                    </div>
+                </div>
+
+        </div>
+    </div> `
+    }
+    ).join('');
+
+    console.log(htmlForCards)
+
+    conatainer.innerHTML = htmlForCards;
+    var cards = document.querySelectorAll('.card');
+
+    cards.forEach(card => {
+        card.addEventListener( 'click', function() {
+            card.classList.toggle('is-flipped');
+                                                    }
+        );
+    })
+  });
 
 //card flip
 
